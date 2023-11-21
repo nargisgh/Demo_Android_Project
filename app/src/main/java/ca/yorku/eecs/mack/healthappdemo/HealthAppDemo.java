cc3d174c7d0126f66eecc75eea27277b84f49bd8
Manav31 <31manavsharma@gmail.com>
2023-11-21 18:16:17 -0500

    Basic ui for the home page implemented and and some of the logic behind

--- app/src/main/java/ca/yorku/eecs/mack/healthappdemo/HealthAppDemo.java
+++ app/src/main/java/ca/yorku/eecs/mack/healthappdemo/HealthAppDemo.java
@@ -1,67 +1,43 @@
 package ca.yorku.eecs.mack.healthappdemo;
-
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
-import java.util.Locale;
 
-import ca.yorku.eecs.mack.demoandroid.R;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class HealthAppDemo extends Activity implements OnClickListener {
-    private final static String MYDEBUG = "MYDEBUG"; // for Log.i messages
+public class HealthAppDemo extends AppCompatActivity {
 
-    private Button incrementButton, decrementButton, exitButton;
-    private TextView textview;
-    private int clickCount;
-
-    // called when the activity is first created
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        initialize();
-        Log.i(MYDEBUG, "Initialization done. Application running.");
-    }
+        setContentView(R.layout.home);
 
-    private void initialize() {
-        // get references to buttons and text view from the layout manager (rather than instantiate them)
-        incrementButton = (Button) findViewById(R.id.incbutton);
-        decrementButton = (Button) findViewById(R.id.decbutton);
-        exitButton = (Button) findViewById(R.id.exitbutton);
-        textview = (TextView) findViewById(R.id.textview);
+        Button exerciseButton = findViewById(R.id.exerciseButton);
+        exerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HealthAppDemo.this, ExerciseActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        // some code is missing here
+        Button sleepButton = findViewById(R.id.sleepButton);
+        sleepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HealthAppDemo.this, SleepActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        // initialize the click count
-        clickCount = 0;
-
-        // initialize the text field with the click count
-        textview.setText(String.format(Locale.CANADA, "Count: %d", clickCount));
-    }
-
-    // this code executes when a button is clicked (i.e., tapped with user's finger)
-    @Override
-    public void onClick(View v) {
-        if (v == incrementButton) {
-            Log.i(MYDEBUG, "Increment button clicked!");
-            ++clickCount;
-
-        } else if (v == decrementButton) {
-            Log.i(MYDEBUG, "Decrement button clicked!");
-            --clickCount;
-
-        } else if (v == exitButton) {
-            Log.i(MYDEBUG, "Good bye!");
-            this.finish();
-
-        } else
-            Log.i(MYDEBUG, "Oops: Invalid Click Event!");
-
-        // update click count
-        textview.setText(String.format(Locale.CANADA, "Count: %d", clickCount));
+        Button mealButton = findViewById(R.id.mealButton);
+        mealButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HealthAppDemo.this, MealActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
