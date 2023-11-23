2de0369e8118b6a699c7fd437f461429b41c8e6c
MAAN31 <Manav31@my.yorku.ca>
2023-11-23 15:17:03 -0500

    Home Page Complete

--- app/src/main/java/ca/yorku/eecs/mack/healthappdemo/HealthAppDemo.java
+++ app/src/main/java/ca/yorku/eecs/mack/healthappdemo/HealthAppDemo.java
@@ -1,43 +1,58 @@
-package ca.yorku.eecs.mack.healthappdemo;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+package ca.yorku.eecs.mack.healthappdemo;// MainActivity.java
 
+import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class HealthAppDemo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_main);
 
-        Button exerciseButton = findViewById(R.id.exerciseButton);
-        exerciseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HealthAppDemo.this, ExerciseActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Get the TextView reference
+        TextView todayDateTextView = findViewById(R.id.todayDateTextView);
 
-        Button sleepButton = findViewById(R.id.sleepButton);
-        sleepButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HealthAppDemo.this, SleepActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Get today's date
+        String todayDate = getCurrentDate();
 
-        Button mealButton = findViewById(R.id.mealButton);
-        mealButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HealthAppDemo.this, MealActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Set the text of todayDateTextView to display today's date
+        todayDateTextView.setText(todayDate);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewExercises);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Create a list of exercises (replace this with your actual data)
+        List<String> exerciseList = new ArrayList<>();
+        exerciseList.add("• Mix it up: Combine cardio and strength training for a well-rounded fitness routine.");
+        exerciseList.add("• Flexibility matters: Include yoga or Pilates for improved mobility.");
+        exerciseList.add("• Consistency beats intensity: Regular, moderate exercise is key.");
+        exerciseList.add("• Function over form: Prioritize exercises that mimic daily movements.");
+        exerciseList.add("• Listen to your body: Adjust workouts based on how you feel.");
+        // Add more exercises as needed
+
+        // Create and set the adapter
+        ExerciseAdapter exerciseAdapter = new ExerciseAdapter(this, exerciseList);
+        recyclerView.setAdapter(exerciseAdapter);
     }
+
+    // Helper method to get today's date
+    private String getCurrentDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        return dateFormat.format(new Date());
+    }
+
 }
